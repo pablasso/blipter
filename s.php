ffab8beccd9a795e9173dffd07d8159c627bf754
Juan Pablo Ortiz Aréchiga <pablasso@gmail.com>
2011-04-02 08:47:31 -0600

    Log modifications

--- s.php
+++ s.php
@@ -1,6 +1,8 @@
 <?php
 
 if (empty($_GET['c']) || !is_numeric($_GET['c']) {
+	file_put_contents("/tmp/gitpost", "INVALID CHANNEL", FILE_APPEND);
+	file_put_contents("/tmp/gitpost", print_r($_REQUEST, true), FILE_APPEND);
 	exit();
 }
 
@@ -12,19 +14,16 @@ if (!empty($_POST['payload'])) {
 	$github_payload = json_decode($_POST['payload'], true);
 }
 
-if ($type == null) {
-	exit();
-}
-
-switch ($type) {
+switch ($service) {
 	case 'github':
-		
 		$author = $github_payload['commits'][0]['author']['email'];
 		$action = "commit on ";
 		$subject = $github_payload['repository']['name'];
 		$description = $github_payload['commits'][0]['message'];
 		break;
 	default:
+		file_put_contents("/tmp/gitpost", "INVALID SERVICE", FILE_APPEND);
+		file_put_contents("/tmp/gitpost", print_r($_REQUEST, true), FILE_APPEND);
 		exit();
 		break;
 }
@@ -44,5 +43,8 @@ define('PUSHER_APP_ID', '4784');
 
 $pusher = new Pusher(PUSHER_API_KEY, PUSHER_API_SECRET, PUSHER_APP_ID);
 $pusher->trigger($channel, 'push', json_encode($to_encode));
+
+file_put_contents("/tmp/gitpost", "PUSHED!", FILE_APPEND);
+file_put_contents("/tmp/gitpost", print_r($_REQUEST, true), FILE_APPEND);
 
 ?>
